3bc14b534735dbfad9d707f0acb6950bc82bd1e5
Magdalena Huk <Magdalena.Huk@brightone.pl>
2018-01-03 14:09:05 +0100

    remove magic numbers, add format of text

--- main.py
+++ main.py
@@ -4,16 +4,26 @@ import datetime
 
 global no_category
 global wrong_value
-wrong_value = "Error occur. Not correct value."
-no_category = "Category doesn't exist"
+global no_file
+global add_correct
+
+wrong_value = "Not correct value."
+no_category = "Category doesn't exist."
 no_file = "File not found."
+add_correct = "Successfully added"
+rm_correct = "Successfully removed"
 
 class CategoryManager:
 
     def __init__(self):
         self.file_name = "CategoriesList.txt"
+        self.add_cat = 1
+        self.remove_cat = 2
+        self.get_cat = 3
+        self.exit = 4
+        self.min_size = 2
 
-    def check_file(self):
+    def get_categories(self):
         """Return list of categories"""
         if os.path.isfile(self.file_name) :
             text = open(self.file_name, "r+")
@@ -24,45 +34,35 @@ class CategoryManager:
         else:
             print(no_file)
 
-    def categories(self):
-        category = self.check_file()
-        return category
-
     def add_category(self):
         """Add category to CategoriesList"""
-        while True:
-            category_name = raw_input("Enter a new category")
-            if category_name in self.check_file():
-                print("Category already exist")
-                break
-            else:
-                cat = open(self.file_name, 'a')
-                cat.write("\n")
-                cat.write(category_name)
-                cat.close()
-                print("Category is added")
-                break
+        category_name = raw_input("Enter a new category")
+        if category_name in self.get_categories():
+            print("Category already exist")
+        else:
+            cat = open(self.file_name, 'a')
+            cat.write("\n")
+            cat.write(category_name)
+            cat.close()
+            print(add_correct)
 
     def remove_category(self):
         """Remove category from CategoriesList"""
         statinfo = os.stat(self.file_name)
-        if statinfo.st_size > 2:
-            while True:
-                category_name = raw_input("Enter category to remove")
-                if category_name not in self.check_file():
-                    print(no_category)
-                    break
-                else:
-                    f = open(self.file_name, "r+")
-                    d = f.readlines()
-                    f.seek(0)
-                    for i in d:
-                        if i != category_name + '\n' and i != category_name:
-                            f.write(i)
-                    f.truncate()
-                    f.close()
-                    print("Category is removed")
-                    break
+        if statinfo.st_size > self.min_size:
+            category_name = raw_input("Enter category to remove")
+            if category_name not in self.get_categories():
+                print(no_category)
+            else:
+                f = open(self.file_name, "r+")
+                d = f.readlines()
+                f.seek(0)
+                for i in d:
+                    if i != category_name + '\n' and i != category_name:
+                        f.write(i)
+                f.truncate()
+                f.close()
+                print(rm_correct)
         else:
             print("File is empty")
 
@@ -74,16 +74,16 @@ class CategoryManager:
                 Press 4 if you want to exit from edit
                 """
         while True:
-            choose = raw_input(text_menu)
-            if int(choose) == 1:
-                self.check_file()
+            choose = int(raw_input(text_menu))
+            if choose == self.add_cat:
+                self.get_categories()
                 self.add_category()
-            elif int(choose) == 2:
+            elif choose == self.remove_cat:
                 self.remove_category()
-            elif int(choose) == 3:
-                for category in self.check_file():
+            elif choose == self.get_cat:
+                for category in self.get_categories():
                     print(category)
-            elif int(choose) == 4:
+            elif choose == self.exit:
                 print("Exit from editor")
                 break
             else:
@@ -91,6 +91,15 @@ class CategoryManager:
 
 
 class ExpenseManager:
+
+    def __init__(self):
+        self.add_exp = 1
+        self.remove_exp = 2
+        self.get_exp_month = 3
+        self.get_exp_cat = 4
+        self.exit = 5
+        self.min_year = 2016
+        self.max_year = 2050
 
     def menu(self):
         text_menu = """ 
@@ -101,24 +110,21 @@ class ExpenseManager:
                         Press 5 if you want to exit expense manager
                         """
         while True:
-            choose = raw_input( text_menu )
-            try:
-                if int(choose) == 1:
-                    self.add_expense()
-                elif int(choose) == 2:
-                    self.remove_expense_date()
-                elif int(choose) == 3:
-                    txt = self.print_expenses_by_month()
-                    print(txt)
-                elif int(choose) == 4:
-                    txt = self.print_expenses_by_category()
-                    print(txt)
-                elif int(choose) == 5:
-                    print("Exit")
-                    break
-                else:
-                    print(wrong_value)
-            except(TypeError,ValueError):
+            choose = int(raw_input(text_menu))
+            if choose == self.add_exp:
+                self.add_expense()
+            elif choose == self.remove_exp:
+                self.remove_expense_date()
+            elif choose == self.get_exp_month:
+                txt = self.print_expenses_by_month()
+                print(txt)
+            elif choose == self.get_exp_cat:
+                txt = self.print_expenses_by_category()
+                print(txt)
+            elif choose == self.exit:
+                print("Exit")
+                break
+            else:
                 print(wrong_value)
 
     def add_expense(self):
@@ -133,33 +139,42 @@ class ExpenseManager:
             str_content = str(content_of_expense)
             add_e.write(str_content)
             add_e.close()
+            print(add_correct)
         except (IOError, TypeError,ValueError):
             print(no_file)
 
     def remove_expense_date(self):
-        """Remove an expense from file as a year"""
-        while True:
-            year, month, day = self.date_expense()
-            name, amount = self.detail_expense()
-            content_of_expense = year, month, day, name, amount
-            str_content = str(content_of_expense)
-            file_name = str(year) + '.txt'
-            if os.path.isfile(file_name):
-                f = open(file_name, "r")
-                lines = f.readlines()
-                f.close()
-                f = open(file_name, "w")
-                for i in lines:
-                    if i != str_content + '\n' and i != str_content:
-                        f.write(i)
-                f.close()
-                break
-            else:
-                print("File not found.")
+        year, month, day = self.date_expense()
+        name, amount = self.detail_expense()
+        content_of_expense = year, month, day, name, amount
+        str_content = str(content_of_expense)
+        file_name = str(year) + '.txt'
+        if os.path.isfile(file_name):
+            f = open(file_name, "r+")
+            lines = f.readlines()
+            f.seek(0)
+            for i in lines:
+                if i != str_content + '\n' and i != str_content:
+                    f.write(i)
+            f.truncate()
+            f.close()
+            print(rm_correct)
+        else:
+            print(no_file)
+
+    def format_txt(self,con_list):
+        """Return formatted list"""
+        formatted_list = con_list.replace('\'', ' ').replace( '(', ' ').replace(')', ' ')
+        return formatted_list
 
     def print_expenses_by_month(self):
         """Show expenses in year by a month"""
-        f = file_object.open_file()
+        while True:
+            f = file_object.open_file()
+            if f is None:
+                print(wrong_value)
+            else:
+                break
         month = raw_input("Enter a month")
         s_month = ' '+month
         find = ""
@@ -173,51 +188,56 @@ class ExpenseManager:
                     a = (''.join(map(str, n)))
                     if a == s_month:
                         find += line
-        return find
+        return self.format_txt(find)
 
     def print_expenses_by_category(self):
         """Show expenses in year by a category"""
-        f = file_object.open_file()
-        category = self.check_category()
+        while True:
+            f = file_object.open_file()
+            if f is None:
+                print(wrong_value)
+            else:
+                break
+        category = self.get_category()
         new_list = []
         for line in f:
             if category in line:
                 new_list.append(line)
-        return ' '.join(map(str, new_list))
+        r_list = ' '.join(map(str, new_list))
+        return self.format_txt(r_list)
 
     def date_expense(self):
         """Return year, month, day"""
         while True:
             year = int(raw_input("Enter a year"))
-            month = int( raw_input("Enter a month"))
-            day = int( raw_input("Enter a day"))
-            if year >= 2016 and year <= 2050:
+            if year >= self.min_year and year <= self.max_year:
+                month = int( raw_input( "Enter a month" ) )
+                day = int( raw_input( "Enter a day" ) )
                 datetime.datetime(year=year, month=month, day=day)
-                file_name = str(year) + '.txt'
-                if os.path.isfile(file_name):
-                    break
-                else:
-                    txt = open(file_name, "w")
-                    txt.close()
+                return year, month, day
             else:
                 print(wrong_value)
-        return year, month, day
 
     def detail_expense(self):
         """Return name and amount of expense """
-        name = self.check_category()
+        name = self.get_category()
         amount = float(raw_input("Enter an amount of expense"))
         return name, amount
 
-    def check_category(self):
+    def get_category(self):
         """Return name of category"""
         name = raw_input("Enter a name of category")
-        if name not in category_object.check_file():
+        if name not in category_object.get_categories():
             print(no_category)
         else:
             return name
 
 class Menu:
+
+    def __init__(self):
+        self.manage_cat = 1
+        self.manage_exp = 2
+        self.manage_exit = 3
 
     def menu_content(self):
         while True:
@@ -228,11 +248,11 @@ class Menu:
                     """)
             choose = raw_input("Enter a number")
             try:
-                if int(choose) == 1:
+                if int(choose) == self.manage_cat:
                     category_object.menu()
-                elif int(choose) == 2:
+                elif int(choose) == self.manage_exp:
                     expense_object.menu()
-                elif int(choose) == 3:
+                elif int(choose) == self.manage_exit:
                     print("Exit")
                     break
                 else:
@@ -251,8 +271,6 @@ class FileManager():
             return f
         else:
             print(no_file)
-
-
 
 
 category_object = CategoryManager()
