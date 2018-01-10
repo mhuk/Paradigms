06742781b16e106981ab36bdbef2a0e6c3c7dd02
Magdalena Huk <Magdalena.Huk@brightone.pl>
2018-01-10 11:44:21 +0100

    Comments

--- mainTest.py
+++ mainTest.py
@@ -1,11 +1,18 @@
-from unittest import TestCase
+#from unittest import TestCase
+from mock import patch
 
-global wrong_value
-wrong_value = "Not correct value."
-
-class TestMenu(TestCase):
-
-    def test_menu(self):
-        self.assertRaises(ValueError,wrong_value, )
-
-
+# global wrong_value
+# wrong_value = "Not correct value."
+#
+# class TestMenu(TestCase):
+#
+#     def test_menu(self):
+#         self.assertRaises(ValueError,wrong_value, )
+# ------------------------------
+# from mock import MagicMock
+# from definitionclass import ExpenseManager
+# thing = ExpenseManager()
+# thing.date_expense = MagicMock(return_value = '2017,10,10')
+# thing.date_expense(2017,10,10,key ='value')
+# thing.date_expense.assert_called_with(2017,10,10,key='value')
+# ------------------------------
